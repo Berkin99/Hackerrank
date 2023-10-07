6b343f56f7748a964f075bea577341acb093981c
Berkin Yılmaz <111027197+Berkin99@users.noreply.github.com>
2023-10-07 20:32:19 +0300

    truckTour.cpp

---
+++ truckTour_MT3.cpp
@@ -0,0 +1,110 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+vector<string> split(const string&);
+
+
+/*
+ * Complete the 'truckTour' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+ */
+
+int truckTour(vector<vector<int>> petrolpumps) {
+    int len = petrolpumps.size();
+    int petrol;
+    int flag = 0;
+    for (int i = 0; i < len; i++) {
+        petrol = 0;
+        flag = 0;
+        for (int j = i; j < (len + i); j++) {
+            int k = j;
+            if (k >= len) k = j - len;
+            petrol += petrolpumps[k][0] - petrolpumps[k][1];
+            if (petrol < 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) return i;
+    }
+    return -1;
+}
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> petrolpumps(n);
+
+    for (int i = 0; i < n; i++) {
+        petrolpumps[i].resize(2);
+
+        string petrolpumps_row_temp_temp;
+        getline(cin, petrolpumps_row_temp_temp);
+
+        vector<string> petrolpumps_row_temp = split(rtrim(petrolpumps_row_temp_temp));
+
+        for (int j = 0; j < 2; j++) {
+            int petrolpumps_row_item = stoi(petrolpumps_row_temp[j]);
+
+            petrolpumps[i][j] = petrolpumps_row_item;
+        }
+    }
+
+    int result = truckTour(petrolpumps);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string& str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
